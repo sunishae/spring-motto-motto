62c5ee9bfde2ec7e53d798950fa96966184ad6ee
Han <jumdo12@gmail.com>
2026-01-10 01:40:45 +0900

    chore: 깃허브 초기 설정 추가

---
+++ src/main/java/jumdo12/springmottomotto/SpringMottoMottoApplication.java
@@ -0,0 +1,13 @@
+package jumdo12.springmottomotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMottoMottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMottoMottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jumdo12/springmottomotto/SpringMottoMottoApplicationTests.java
@@ -0,0 +1,13 @@
+package jumdo12.springmottomotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMottoMottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
