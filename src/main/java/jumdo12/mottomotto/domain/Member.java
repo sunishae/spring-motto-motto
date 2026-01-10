8d73d798ddf24acb2c0c311a94d6a0ab6a345bb8
Han <jumdo12@gmail.com>
2026-01-10 14:59:18 +0900

    feat: Member, BaseEntity 추가

---
+++ src/main/java/jumdo12/mottomotto/domain/Member.java
@@ -0,0 +1,18 @@
+package jumdo12.mottomotto.domain;
+
+import jakarta.persistence.*;
+import jumdo12.mottomotto.common.BaseEntity;
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+}
